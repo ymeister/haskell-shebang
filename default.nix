6df8349df4d4006549e36a02b30da1d5cf098e5b
ymeister <47071325+ymeister@users.noreply.github.com>
2024-10-21 14:33:02 +0900

    Use overlay from haskell-prelude

--- default.nix
+++ default.nix
@@ -8,10 +8,11 @@ let nix-thunk = import ./deps/nix-thunk { inherit pkgs; };
     nix-shebang = import deps.nix-shebang { pkgs = pkgs; };
     haskell-shebang = nix-shebang.haskell;
 
+    prelude-overlay = import (deps.haskell-prelude + "/overlay.nix") { inherit pkgs; };
+    with-prelude-overlay = "${prelude-overlay}/bin/with-prelude-overlay";
+
 in writeScriptBin "haskell-shebang" ''
   #!/bin/sh
 
-  export NIX_PATH="$NIX_PATH:nixpkgs-overlays=${./.}/overlays"
-
-  exec "${haskell-shebang}/bin/nix-haskell-shebang" prelude "$@"
+  exec ${with-prelude-overlay} "${haskell-shebang}/bin/nix-haskell-shebang" prelude "$@"
 ''
